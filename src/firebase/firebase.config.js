de13a2984714ffdaa77c80b70710681f1f204173
sourav-mahmud <souravmahmud32@gmail.com>
2022-10-26 17:33:33 +0600

    fundamental package installed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCH2IkdqAIU0zx1NtEf9jqyZTbouvbbmJM",
+  authDomain: "course-guru-19603.firebaseapp.com",
+  projectId: "course-guru-19603",
+  storageBucket: "course-guru-19603.appspot.com",
+  messagingSenderId: "21471767516",
+  appId: "1:21471767516:web:596238d3fed4d19edf7d19"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
