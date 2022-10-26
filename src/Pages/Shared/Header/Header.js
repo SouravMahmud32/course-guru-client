269cc613e6b149bbea43944e756ddb6e96f4fca7
sourav-mahmud <souravmahmud32@gmail.com>
2022-10-26 22:23:06 +0600

    navbar added

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import LeftSideNav from "../LeftSideNav/LeftSideNav";
+
+const Header = () => {
+  return (
+    <Navbar collapseOnSelect className="mb-4" expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">courseGuru</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">Courses</Nav.Link>
+            <Nav.Link href="#blogs">Blogs</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+          <div className="d-lg-none">
+            <LeftSideNav></LeftSideNav>
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
